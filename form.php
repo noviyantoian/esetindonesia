b1ba2d93a6739e583d793220f3d990e663ed2624
Noviyanto <noviyantoian@gmail.com>
2021-11-11 21:28:11 +0700

    uwo

--- form.php
+++ form.php
@@ -13,7 +13,15 @@ if (isset($_POST['kirim'])) {
   function sendMessage($telegram_id, $nama, $perusahaan, $email, $wa, $jumlahpc, $keterangan, $secret_token)
   {
     $url =  "https://api.telegram.org/bot" . $secret_token . "/sendMessage?=parse_mode=markdown&chat_id=" . $telegram_id;
-    $url = $url . "&text=" . urlencode("Nama Lengkap: " . $nama . "\n Perusahaan/Instansi: " . $perusahaan . "\n Email: " . $email . "\n Telp/WhatsApp: " . $wa . "\n Jumlah PC/Laptop: " . $jumlahpc . "\n Keterangan: " . $keterangan);
+    $text = "Nama Lengkap: <b>" . $nama . "</b></br>";
+    $text = "Perusahaan: <b>" . $nperusahaan . "</b></br>";
+    $text = "email: <b>" . $email . "</b></br>";
+    $text = "Telp/WhatsApp: <b>" . $wa . "</b></br>";
+    $text = "Keterangan Lain: <b><i>" . $keterangan . "</i></b></br>";
+
+    // $url = $url . "&text=" . urlencode("Nama Lengkap: " . $nama . "\n Perusahaan/Instansi: " . $perusahaan . "\n Email: " . $email . "\n Telp/WhatsApp: " . $wa . "\n Jumlah PC/Laptop: " . $jumlahpc . "\n Keterangan: " . $keterangan);
+
+    $url = $url . "&text=" . urlencode($text);
     $ch = curl_init();
 
     $optArray = array(
@@ -40,7 +48,7 @@ if (isset($_POST['kirim'])) {
   $telegram_id = ["470830212", "934672013"];
 
   foreach ($telegram_id as $id_tele) {
-    sendMessage($id_tele, $nama, $perusahaan, $email, $wa, $jumlahpc, $keterangan, $secret_token);
+    sendMessage($id_tele, $namaLengkap, $perusahaan, $email, $wa, $jumlahpc, $keterangan, $secret_token);
   }
   header('location:/success.html');
 }
