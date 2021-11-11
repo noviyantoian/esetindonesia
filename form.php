cc0fd99e0054e6707eeb67066159bdef27210ee3
Noviyanto <noviyantoian@gmail.com>
2021-11-11 19:23:26 +0700

    fix token and id

--- form.php
+++ form.php
@@ -6,23 +6,25 @@ if ($con->connect_errno) {
   exit();
 }
 
-function sendMessage($telegram_id, $message, $secret_token)
-{
-  $url =  "https://api.telegram.org/bot" . $secret_token . "/sendMessage?=parse_mode=markdown&chat_id" . $telegram_id;
-  $url = $url . "&text" . urlencode($message);
-  $ch = curl_init();
 
-  $optArray = array(
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true,
-  );
-
-  curl_setopt_array($ch, $optArray);
-  $result = curl_exec($ch);
-  curl_close($ch);
-}
 
 if (isset($_POST['kirim'])) {
+
+  function sendMessage($telegram_id, $message, $secret_token)
+  {
+    $url =  "https://api.telegram.org/bot" . $secret_token . "/sendMessage?=parse_mode=markdown&chat_id" . $telegram_id;
+    $url = $url . "&text" . urlencode($message);
+    $ch = curl_init();
+
+    $optArray = array(
+      CURLOPT_URL => $url,
+      CURLOPT_RETURNTRANSFER => true,
+    );
+
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+  }
 
   $namaLengkap = trim(htmlentities($_POST['namaLengkap']));
   $perusahaan = trim(htmlentities($_POST['perusahaan']));
