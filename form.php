bdda261293ac9145843d9c4315f7828f0620533c
Noviyanto <noviyantoian@gmail.com>
2021-11-11 19:35:01 +0700

    okes

--- form.php
+++ form.php
@@ -37,7 +37,7 @@ if (isset($_POST['kirim'])) {
   $data = mysqli_query($con, "INSERT INTO penawaran (namaLengkap,perusahaan,email,wa,jumlahPc,keterangan) VALUES ('$namaLengkap','$perusahaan','$email','$wa','$jumlahpc','$keterangan')");
 
   $secret_token = "2143650264:AAE4NWW2kV-sUQpn9MtCRKe7xTFVeP0B18U";
-  $telegram_id = "470830212,934672013";
+  $telegram_id = "934672013";
 
   sendMessage($telegram_id, $keterangan, $secret_token);
 
