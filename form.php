cd36bfe70d9e4ff5449cf56995c2bef757fc5d6b
Noviyanto <noviyantoian@gmail.com>
2021-11-11 15:13:11 +0700

    fix form

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "esetindonesia");
+
+if ($con->connect_errno) {
+  echo "err";
+  exit();
+}
+
+if (isset($_POST['kirim'])) {
+
+  $namaLengkap = trim(htmlentities($_POST['namaLengkap']));
+  $perusahaan = trim(htmlentities($_POST['perusahaan']));
+  $email = trim(htmlentities($_POST['email']));
+  $wa = trim(htmlentities($_POST['wa']));
+  $keterangan = trim(htmlentities($_POST['keterangan']));
+  $jumlahpc = trim(htmlentities($_POST['jumlahPc']));
+
+  $data = mysqli_query($con, "INSERT INTO penawaran (namaLengkap,perusahaan,email,wa,jumlahPc,keterangan) VALUES ('$namaLengkap','$perusahaan','$email','$wa','$jumlahPc','$keterangan')");
+
+  header('location:/index.php?=success');
+}
